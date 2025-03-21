78ca7d1cfd67138b0f5ee4b63fcc52fac8104d5c
lahfaouti <ilahfaouti@gmail.com>
2025-03-21 17:27:00 +0100

    Ajout du fichier wv_views.sql

--- wv_views.sql
+++ wv_views.sql
@@ -1,27 +1,44 @@
-CREATE VIEW ALL_PLAYERS AS
+CREATE VIEW ALL_PLAYERS AS 
 SELECT 
-    p.pseudo AS player_name,
-    COUNT(pip.id_party) AS number_of_parties,
-    SUM(CASE WHEN pip.is_alive = 'yes' THEN 1 ELSE 0 END) AS alive_count
+    p.pseudo AS nom_du_joueur ,
+    COUNT (DISTINCT p_parties.id_party) AS nombre_de_parties,
+    COUNT (p_play.id_turn) AS nombre_de_tours_jouées ,
+    MIN (t.start_time) AS date_premiere_participation,
+    MAX(p_play.end_time) AS date_derniere_action
 FROM players p
-LEFT JOIN players_in_parties pip ON p.id_player = pip.id_player
-GROUP BY p.pseudo;
+JOIN players_in_parties p_parties ON p.id_player = p_parties.id_player
+JOIN turns t ON p_parties.id_party = t.id_party
+LEFT JOIN players_play p_play ON p.id_player = p_play.id_player
+GROUP BY p.pseudo
+ORDER BY nombre_de_parties DESC, date_premiere_participation, date_derniere_action, nom_du_joueur;
 
-CREATE VIEW ALL_PARTIES AS
-SELECT 
-    p.id_party,
-    p.title_party,
-    COUNT(pip.id_player) AS number_of_players
-FROM parties p
-LEFT JOIN players_in_parties pip ON p.id_party = pip.id_party
-GROUP BY p.id_party, p.title_party;
 
-CREATE VIEW ALL_TURNS AS
+-- ALL_PLAYERS_ELAPSED_GAME VIEW --
+CREATE VIEW ALL_PLAYERS_ELAPSED_GAME AS
 SELECT 
-    t.id_turn,
-    p.title_party,
-    t.start_time,
-    t.end_time,
-    DATEDIFF(SECOND, t.start_time, t.end_time) AS duration
-FROM turns t
-JOIN parties p ON t.id_party = p.id_party;
+    p.pseudo AS nom_du_joueur,
+    pa.title_party AS nom_de_la_partie,
+    (SELECT COUNT(DISTINCT p_in_parties.id_player) FROM players_in_parties p_in_parties WHERE p_in_parties.id_party = pa.id_party) AS nombre_de_participants,
+    MIN(p_play.start_time) AS date_premiere_action,
+    MAX(p_play.end_time) AS date_derniere_action,
+    TIMESTAMPDIFF(SECOND, MIN(p_play.start_time), MAX(p_play.end_time)) AS nb_secondes_dans_la_partie
+FROM players p
+JOIN players_in_parties p_parties ON p.id_player = p_parties.
+JOIN parties pa ON p_parties.id_party = pa.id_party
+JOIN players_play p_play ON p.id_player = p_play.id_player
+GROUP BY p.pseudo, pa.title_party, pa.id_party;
+
+
+-- ALL_PLAYERS_ELAPSED_TOUR VIEW --
+CREATE VIEW ALL_PLAYERS_ELAPSED_TOUR AS
+SELECT 
+    p.pseudo AS nom_du_joueur,
+    pa.title_party AS nom_de_la_partie,
+    t.id_turn AS numero_du_tour,
+    t.start_time AS debut_du_tour,
+    p_play.start_time AS decision_du_joueur,
+    TIMESTAMPDIFF(SECOND, t.start_time, p_play.start_time) AS temps_ecoule_pour_joueur
+FROM players p
+JOIN players_play p_play ON p.id_player = p_play.id_player
+JOIN turns t ON p_play.id_turn = t.id_turn
+JOIN parties pa ON t.id_party = pa.id_party;
