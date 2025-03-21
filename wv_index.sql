5bcbb80d9d7945a7dd285dc58b7b4439ca3bf8e4
kaidi <marouanekaidigmail.com>
2025-03-21 17:42:17 +0100

    Ajout de wv_index.sql

--- wv_index.sql
+++ wv_index.sql
@@ -1,15 +1,23 @@
--- Index pour la table players_in_parties sur la colonne id_party et id_player
-CREATE INDEX idx_players_in_parties_party_player
-ON players_in_parties (id_party, id_player);
+-- Création d'un index sur les pseudos des joueurs pour accélérer les recherches
+CREATE INDEX idx_players_pseudo ON players (pseudo);
+GO
 
--- Index pour la table turns sur la colonne id_party pour améliorer les requêtes liées aux tours
-CREATE INDEX idx_turns_party
-ON turns (id_party);
+-- Création d'un index sur le titre des parties pour accélérer les recherches
+CREATE INDEX idx_parties_title ON parties (title_party);
+GO
 
--- Index pour la table players_in_parties sur la colonne is_alive, utile pour les requêtes sur l'état des joueurs
-CREATE INDEX idx_players_in_parties_alive
-ON players_in_parties (is_alive);
+-- Index sur la table `players_in_parties` pour optimiser les recherches par joueur et partie
+CREATE INDEX idx_players_in_parties ON players_in_parties (id_player, id_party);
+GO
 
--- Index pour la table players_play sur la colonne id_turn, afin de faciliter la recherche des actions des joueurs par tour
-CREATE INDEX idx_players_play_turn
-ON players_play (id_turn);
+-- Index sur la table `players_play` pour accélérer les recherches par joueur et tour
+CREATE INDEX idx_players_play ON players_play (id_player, id_turn);
+GO
+
+-- Index sur la table `turns` pour accélérer les recherches des tours par partie
+CREATE INDEX idx_turns_party ON turns (id_party, start_time);
+GO
+
+-- Index pour optimiser la récupération des actions d’un joueur dans une partie
+CREATE INDEX idx_players_play_action ON players_play (id_player, action);
+GO
