e49e7d89d585898f8ee170fcd08f15e39c7301ec
kaidi <marouanekaidigmail.com>
2025-03-21 17:55:39 +0100

    Ajout de wv_procs.sql

--- wv_procs.sql
+++ wv_procs.sql
@@ -1,49 +1,92 @@
--- Procédure pour ajouter un nouveau joueur
-CREATE PROCEDURE dbo.AddPlayer
-    @pseudo VARCHAR(100)
+CREATE PROCEDURE SEED_DATA
+    @NB_PLAYERS INT,
+    @PARTY_ID INT
 AS
 BEGIN
-    INSERT INTO players (pseudo)
-    VALUES (@pseudo);
+    DECLARE @tour_count INT;
+    DECLARE @max_tours INT;
+    
+    -- Récupération du nombre maximum de tours pour cette partie
+    SELECT @max_tours = COUNT(*) FROM turns WHERE id_party = @PARTY_ID;
+    
+    -- Initiation du compteur à 0
+    SET @tour_count = 0;
+    
+    -- Création de plusieurs tours que nécessaire
+    WHILE @tour_count < @max_tours
+    BEGIN
+        -- Créeation d'un nouveau tour
+        INSERT INTO turns (id_party, start_time, end_time)
+        VALUES (@PARTY_ID, GETDATE(), DATEADD(MINUTE, 5, GETDATE()));
+        
+        -- Incrémentation du compteur
+        SET @tour_count = @tour_count + 1;
+    END;
 END;
-GO
+/*
+test de la procédure
+-- EXEC SEED_DATA 5, 1;
+*/
 
--- Procédure pour ajouter un joueur à une partie avec un rôle
-CREATE PROCEDURE dbo.AddPlayerToGame
-    @player_id INT,
-    @party_id INT,
-    @role_id INT
+CREATE PROCEDURE COMPLETE_TOUR
+    @TOUR_ID INT,
+    @PARTY_ID INT
 AS
 BEGIN
-    INSERT INTO players_in_parties (id_party, id_player, id_role, is_alive)
-    VALUES (@party_id, @player_id, @role_id, 1);
+    DECLARE @player_id INT;
+    DECLARE @action VARCHAR(10);
+    DECLARE @origin_position_col VARCHAR(255);
+    DECLARE @origin_position_row VARCHAR(255);
+    DECLARE @target_position_col VARCHAR(255);
+    DECLARE @target_position_row VARCHAR(255);
+    
+    -- Récupération des actions des joueurs pour ce tour
+    DECLARE actions CURSOR FOR
+    SELECT 
+        id_player,
+        action,
+        origin_position_col,
+        origin_position_row,
+        target_position_col,
+        target_position_row
+    FROM players_play
+    WHERE id_turn = @TOUR_ID;
+    
+    -- Ouverture du curseur
+    OPEN actions;
+    
+    -- Lecture de la première ligne
+    FETCH NEXT FROM actions INTO @player_id, @action, @origin_position_col, @origin_position_row, @target_position_col, @target_position_row;
+    
+    -- si il y'a d'autre ligne
+    WHILE @@FETCH_STATUS = 0
+    BEGIN
+        -- Mise à jour de la position du joueur
+        UPDATE players_play
+        SET end_time = GETDATE()
+        WHERE id_player = @player_id AND id_turn = @TOUR_ID;
+        
+        -- Lire la ligne suivant
+        FETCH NEXT FROM actions INTO @player_id, @action, @origin_position_col, @origin_position_row, @target_position_col, @target_position_row;
+    END;
+    
+    -- terminer l'action du curseur 
+    CLOSE actions;
+    DEALLOCATE actions;
 END;
-GO
+/*
+test de la procédure
+-- EXEC COMPLETE_TOUR 1, 1;
+*/
 
--- Procédure pour enregistrer une action d’un joueur dans un tour
-CREATE PROCEDURE dbo.RecordPlayerAction
-    @player_id INT,
-    @turn_id INT,
-    @action VARCHAR(20),
-    @origin_col INT,
-    @origin_row INT,
-    @target_col INT,
-    @target_row INT
+CREATE PROCEDURE USERNAME_TO_LOWER
 AS
 BEGIN
-    INSERT INTO players_play (id_player, id_turn, action, origin_position_col, origin_position_row, target_position_col, target_position_row)
-    VALUES (@player_id, @turn_id, @action, @origin_col, @origin_row, @target_col, @target_row);
+    --utilisation des polices minuscules 
+    UPDATE players
+    SET pseudo = LOWER(pseudo);
 END;
-GO
-
--- Procédure pour marquer un joueur comme mort dans une partie
-CREATE PROCEDURE dbo.MarkPlayerAsDead
-    @player_id INT,
-    @party_id INT
-AS
-BEGIN
-    UPDATE players_in_parties
-    SET is_alive = 0
-    WHERE id_player = @player_id AND id_party = @party_id;
-END;
-GO
+/*
+test de la procédure
+-- EXEC USERNAME_TO_LOWER;
+*/
