2525b8dcd5d224485466dd4acc7741238464b5ab
kaidi <marouanekaidigmail.com>
2025-03-21 17:42:41 +0100

    Ajout de wv_procs.sql

--- wv_procs.sql
+++ wv_procs.sql
@@ -1,31 +1,49 @@
-CREATE PROCEDURE SEED_DATA(@NB_PLAYERS INT, @PARTY_ID INT)
+-- Procédure pour ajouter un nouveau joueur
+CREATE PROCEDURE dbo.AddPlayer
+    @pseudo VARCHAR(100)
 AS
 BEGIN
-    DECLARE @i INT = 0;
-    WHILE @i < @NB_PLAYERS
-    BEGIN
-        INSERT INTO players (id_player, pseudo) 
-        VALUES (@i + 1, 'Player' + CAST(@i AS VARCHAR));
-        
-        INSERT INTO players_in_parties (id_party, id_player, id_role, is_alive) 
-        VALUES (@PARTY_ID, @i + 1, dbo.get_random_role(), 'yes');
-        
-        SET @i = @i + 1;
-    END
+    INSERT INTO players (pseudo)
+    VALUES (@pseudo);
 END;
+GO
 
-CREATE PROCEDURE COMPLETE_TURN(@TURN_ID INT)
+-- Procédure pour ajouter un joueur à une partie avec un rôle
+CREATE PROCEDURE dbo.AddPlayerToGame
+    @player_id INT,
+    @party_id INT,
+    @role_id INT
 AS
 BEGIN
-    UPDATE players_play
-    SET end_time = GETDATE()
-    WHERE id_turn = @TURN_ID;
+    INSERT INTO players_in_parties (id_party, id_player, id_role, is_alive)
+    VALUES (@party_id, @player_id, @role_id, 1);
 END;
+GO
 
-CREATE PROCEDURE KILL_PLAYER(@PLAYER_ID INT, @PARTY_ID INT)
+-- Procédure pour enregistrer une action d’un joueur dans un tour
+CREATE PROCEDURE dbo.RecordPlayerAction
+    @player_id INT,
+    @turn_id INT,
+    @action VARCHAR(20),
+    @origin_col INT,
+    @origin_row INT,
+    @target_col INT,
+    @target_row INT
 AS
 BEGIN
-    UPDATE players_in_parties 
-    SET is_alive = 'no' 
-    WHERE id_player = @PLAYER_ID AND id_party = @PARTY_ID;
+    INSERT INTO players_play (id_player, id_turn, action, origin_position_col, origin_position_row, target_position_col, target_position_row)
+    VALUES (@player_id, @turn_id, @action, @origin_col, @origin_row, @target_col, @target_row);
 END;
+GO
+
+-- Procédure pour marquer un joueur comme mort dans une partie
+CREATE PROCEDURE dbo.MarkPlayerAsDead
+    @player_id INT,
+    @party_id INT
+AS
+BEGIN
+    UPDATE players_in_parties
+    SET is_alive = 0
+    WHERE id_player = @player_id AND id_party = @party_id;
+END;
+GO
