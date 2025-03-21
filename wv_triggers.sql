d185defb93a357ce41df8968ef677004836e1c4f
kaidi <marouanekaidigmail.com>
2025-03-21 17:43:42 +0100

    Ajout de wv_triggers.sql

--- wv_triggers.sql
+++ wv_triggers.sql
@@ -1,21 +1,62 @@
-CREATE TRIGGER trg_UpdateTurn
-ON turns
+-- Trigger pour empêcher un joueur de rejoindre une partie s'il y est déjà inscrit
+CREATE TRIGGER trg_PreventDuplicatePlayers
+ON players_in_parties
+FOR INSERT
+AS
+BEGIN
+    IF EXISTS (
+        SELECT 1
+        FROM players_in_parties pip
+        JOIN inserted i ON pip.id_party = i.id_party AND pip.id_player = i.id_player
+    )
+    BEGIN
+        RAISERROR ('Le joueur est déjà inscrit dans cette partie.', 16, 1);
+        ROLLBACK;
+    END;
+END;
+GO
+
+-- Trigger pour empêcher qu’un joueur effectue plusieurs actions dans le même tour
+CREATE TRIGGER trg_LimitPlayerActionsPerTurn
+ON players_play
+FOR INSERT
+AS
+BEGIN
+    IF EXISTS (
+        SELECT 1
+        FROM inserted i
+        JOIN players_play p ON i.id_player = p.id_player AND i.id_turn = p.id_turn
+    )
+    BEGIN
+        RAISERROR ('Un joueur ne peut effectuer qu''une seule action par tour.', 16, 1);
+        ROLLBACK;
+    END;
+END;
+GO
+
+-- Trigger pour mettre à jour la date de fin d'un tour quand tous les joueurs ont joué
+CREATE TRIGGER trg_UpdateTurnEndTime
+ON players_play
 AFTER INSERT
 AS
 BEGIN
-    DECLARE @PARTY_ID INT;
-    SELECT @PARTY_ID = inserted.id_party FROM inserted;
+    DECLARE @id_turn INT, @id_party INT, @total_players INT, @played_count INT;
     
-    -- Logique pour démarrer le tour ou notifier les joueurs
-END;
+    SELECT @id_turn = id_turn FROM inserted;
+    SELECT @id_party = t.id_party FROM turns t WHERE t.id_turn = @id_turn;
 
-CREATE TRIGGER trg_PlayerStatusChange
-ON players_in_parties
-AFTER UPDATE
-AS
-BEGIN
-    DECLARE @PLAYER_ID INT;
-    SELECT @PLAYER_ID = inserted.id_player FROM inserted;
-    
-    -- Logique pour gérer le changement de statut du joueur
+    -- Nombre total de joueurs dans la partie
+    SELECT @total_players = COUNT(*) FROM players_in_parties WHERE id_party = @id_party;
+
+    -- Nombre de joueurs ayant déjà joué ce tour
+    SELECT @played_count = COUNT(DISTINCT id_player) FROM players_play WHERE id_turn = @id_turn;
+
+    -- Si tous les joueurs ont joué, on clôture le tour
+    IF @played_count = @total_players
+    BEGIN
+        UPDATE turns
+        SET end_time = GETDATE()
+        WHERE id_turn = @id_turn;
+    END;
 END;
+GO
